ff28c5935c4940defa5e3ef7007e439b1aa95104
Gabrielle Lowe <97476695+lowe0142@users.noreply.github.com>
2023-04-13 15:32:26 -0400

    adjuts javascript and date button

---
+++ main.js
@@ -0,0 +1,25 @@
+function getAPOD(date, apiKey) {
+  const apiUrl = `https://api.nasa.gov/planetary/apod?date=${date}&api_key=${apiKey}`;
+
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const imageUrl = data.url;
+      const title = data.title;
+      const explanation = data.explanation;
+
+      // update the HTML elements on the page to display the Picture of the Day
+      document.getElementById('apod-image').src = imageUrl;
+      document.getElementById('apod-image').alt = title;
+      document.getElementById('image-title').textContent = title;
+      document.getElementById('image-explanation').textContent = explanation;
+    })
+    .catch(error => console.error(error));
+}
+const launchButton = document.getElementById('launch-button');
+const apiKey = '8neWwEflI1jIW6pOV2tUxoUvEaFDShETxqZakc7R';
+
+launchButton.addEventListener('click', () => {
+  const date = document.getElementById('date-input').value;
+  getAPOD(date, apiKey);
+});
