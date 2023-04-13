9bd9d2df69fce7e352c93361a225e71cca429481
Gabrielle Lowe <97476695+lowe0142@users.noreply.github.com>
2023-04-13 16:22:33 -0400

    deletes error star

--- main.js
+++ main.js
@@ -13,9 +13,18 @@ function getAPOD(date, apiKey) {
       document.getElementById('apod-image').alt = title;
       document.getElementById('apod-title').textContent = title;
       document.getElementById('apod-explanation').textContent = explanation;
+
+      // add event listener to favorite button to save current image URL to a cookie
+      const favoriteButton = document.getElementById('favorite-button');
+      favoriteButton.addEventListener('click', () => {
+        const favoriteImages = getFavoriteImages();
+        favoriteImages.push(imageUrl);
+        setFavoriteImages(favoriteImages);
+      });
     })
     .catch(error => console.error(error));
 }
+
 
 const launchButton = document.getElementById('launch-button');
 const apiKey = '8neWwEflI1jIW6pOV2tUxoUvEaFDShETxqZakc7R';
@@ -25,16 +34,3 @@ launchButton.addEventListener('click', () => {
   getAPOD(date, apiKey);
 });
 
-function addToFavorites() {
-  const imageUrl = document.getElementById('apod-image').src;
-  const title = document.getElementById('apod-title').textContent;
-  const explanation = document.getElementById('apod-explanation').textContent;
-  const apodData = { imageUrl, title, explanation };
-  const date = document.getElementById('date-input').value;
-  const favorites = JSON.parse(localStorage.getItem('favorites') || '{}');
-  favorites[date] = apodData;
-  localStorage.setItem('favorites', JSON.stringify(favorites));
-}
-
-const addToFavoritesButton = document.getElementById('add-to-favorites');
-addToFavoritesButton.addEventListener('click', addToFavorites);
