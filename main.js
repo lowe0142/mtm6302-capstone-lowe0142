689130fad2f81fc2e74945749ed1e06c494d039b
Gabrielle Lowe <97476695+lowe0142@users.noreply.github.com>
2023-04-15 08:57:01 -0400

    completes this assignment

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function getAPOD(date, apiKey) {
       const title = data.title;
       const explanation = data.explanation;
 
-      // update the HTML elements on the page to display the Picture of the Day
+    
       document.getElementById('apod-image').src = imageUrl;
       document.getElementById('apod-image').alt = title;
       document.getElementById('apod-title').textContent = title;
@@ -16,7 +16,6 @@ function getAPOD(date, apiKey) {
 
       document.getElementById('apod-image').style.display = 'block';
 
-      // add event listener to favorite button to save current image URL to a cookie
       const favoriteButton = document.getElementById('favorite-button');
       favoriteButton.addEventListener('click', () => {
         const favoriteImages = getFavoriteImages();
@@ -42,23 +41,20 @@ let favoriteImages = [];
 
 
 function addToFavorites() {
-  // Get the current image's data
+  // Get Image's data
   const apodImage = document.getElementById('apod-image').src;
   const apodTitle = document.getElementById('apod-title').textContent;
   const apodExplanation = document.getElementById('apod-explanation').textContent;
 
 
-  // Create an object to store the image data
   const favoriteImage = {
     image: apodImage,
     title: apodTitle,
     explanation: apodExplanation
   };
 
-  // Check if the image is already in the favoriteImages array
   const existingImage = favoriteImages.find(img => img.title === favoriteImage.title);
 
-  // If the image is not in the array, add it to the array and save to localStorage
   if (!existingImage) {
     favoriteImages.push(favoriteImage);
     localStorage.setItem('favoriteImages', JSON.stringify(favoriteImages));
@@ -72,14 +68,11 @@ const favoriteButton = document.getElementById('favorite-button');
 favoriteButton.addEventListener('click', addToFavorites);
 
 function showFavorites() {
-  // Get the saved favorite images from localStorage
   const savedFavorites = localStorage.getItem('favoriteImages');
 
-  // If there are saved favorite images, parse the JSON and display them
   if (savedFavorites) {
     favoriteImages = JSON.parse(savedFavorites);
 
-    // Loop through the favoriteImages array and display each image
     favoriteImages.forEach(image => {
       const imageContainer = document.createElement('div');
       imageContainer.classList.add('favorite-image');
