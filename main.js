43635bb739a584db816315708905a0c31491b039
Gabrielle Lowe <97476695+lowe0142@users.noreply.github.com>
2023-04-15 08:06:45 -0400

    adds favourite button

--- main.js
+++ main.js
@@ -33,3 +33,70 @@ launchButton.addEventListener('click', () => {
   const date = document.getElementById('date-input').value;
   getAPOD(date, apiKey);
 });
+
+//FAVOURITE BUTTON//
+
+let favoriteImages = [];
+
+
+function addToFavorites() {
+  // Get the current image's data
+  const apodImage = document.getElementById('apod-image').src;
+  const apodTitle = document.getElementById('apod-title').textContent;
+  const apodExplanation = document.getElementById('apod-explanation').textContent;
+
+  // Create an object to store the image data
+  const favoriteImage = {
+    image: apodImage,
+    title: apodTitle,
+    explanation: apodExplanation
+  };
+
+  // Check if the image is already in the favoriteImages array
+  const existingImage = favoriteImages.find(img => img.title === favoriteImage.title);
+
+  // If the image is not in the array, add it to the array and save to localStorage
+  if (!existingImage) {
+    favoriteImages.push(favoriteImage);
+    localStorage.setItem('favoriteImages', JSON.stringify(favoriteImages));
+    alert('Image added to favorites!');
+  } else {
+    alert('Image already in favorites!');
+  }
+}
+
+const favoriteButton = document.getElementById('favorite-button');
+favoriteButton.addEventListener('click', addToFavorites);
+
+function showFavorites() {
+  // Get the saved favorite images from localStorage
+  const savedFavorites = localStorage.getItem('favoriteImages');
+
+  // If there are saved favorite images, parse the JSON and display them
+  if (savedFavorites) {
+    favoriteImages = JSON.parse(savedFavorites);
+
+    // Loop through the favoriteImages array and display each image
+    favoriteImages.forEach(image => {
+      const imageContainer = document.createElement('div');
+      imageContainer.classList.add('favorite-image');
+
+      const imageTitle = document.createElement('h3');
+      imageTitle.textContent = image.title;
+
+      const imageExplanation = document.createElement('p');
+      imageExplanation.textContent = image.explanation;
+
+      const imageElement = document.createElement('img');
+      imageElement.src = image.image;
+
+      imageContainer.appendChild(imageTitle);
+      imageContainer.appendChild(imageElement);
+      imageContainer.appendChild(imageExplanation);
+
+      document.getElementById('favorites-container').appendChild(imageContainer);
+    });
+  }
+}
+
+window.onload = showFavorites;
